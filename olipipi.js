a91e35e9ee4f3c56ba51588aa2658ca9ffcdd262
Olipipishub <152216649+Olipipishub@users.noreply.github.com>
2024-05-07 11:11:05 -0400

    Update olipipi.js

--- olipipi.js
+++ olipipi.js
@@ -1,3 +1,13 @@
 function navigateToWebsite(url) {
     window.location.href = url;
 }
+
+function passwordPrompt() {
+    var password = prompt("Please enter the password:");
+    if (password === "Potato") {
+        // Unhide buttons or show content if the password is correct
+        document.getElementById("buttons").style.display = "block";
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
