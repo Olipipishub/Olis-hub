c8e67ad331533b8e53933f97a306b3f4183c6a56
Olipipishub <152216649+Olipipishub@users.noreply.github.com>
2025-02-05 07:55:30 -0500

    Update olipipi.js

--- olipipi.js
+++ olipipi.js
@@ -5,8 +5,7 @@ function navigateToWebsite(url) {
 function passwordPrompt() {
     var password = prompt("Please enter the password:");
     if (password === "Potato") {
-        // Unhide buttons or show content if the password is correct
-        document.getElementById("buttons").style.display = "block";
+        document.getElementById("buttons").style.display = "flex";
     } else {
         alert("Incorrect password. Please try again.");
     }
